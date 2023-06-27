5c51fdce3f61617465c5d619ad62df9d650fa699
Nugget-Meister <tremainecamacho@gmail.com>
2023-06-26 20:57:08 -0400

    create basic js

---
+++ script.js
@@ -0,0 +1,19 @@
+const form = document.querySelector("form")
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let name = e.target.name.value
+    let price = e.target.price.value
+    let color = e.target.color.value
+    let size = e.target.size.value
+    let inStock = e.target.size.value
+
+   
+})
+
+
+function itemTemplate(name, price, color, size, inStock) {
+
+    
+}
+function createItem(){}
