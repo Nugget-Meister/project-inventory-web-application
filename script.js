303c67de3c1dd7c56955e3cf10ccd6f14fe792c5
Nugget-Meister <tremainecamacho@gmail.com>
2023-06-27 19:49:43 -0400

    fix adding li to ul, switch size & add options

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const form = document.querySelector("form")
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
+
     let name = e.target.name.value
     let price = e.target.price.value
     let color = e.target.color.value
@@ -9,11 +10,25 @@ form.addEventListener("submit", (e) => {
     let inStock = e.target.size.value
 
    
+    createItem(name,price,color,size,inStock)
 })
 
 
 function itemTemplate(name, price, color, size, inStock) {
+    let li = document.createElement("li")
 
-    
+    // let nameObj = document.createElement()
+    // let priceObj
+    // let colorObj
+    // let sizeObj
+    // let inStockObj
+
+    return li
 }
-function createItem(){}
+
+function createItem(name, price, color, size, inStock){
+    const ul = document.querySelector("ul");
+    const li = itemTemplate(name,price,color,size,inStock)
+
+    ul.append(li)
+}
